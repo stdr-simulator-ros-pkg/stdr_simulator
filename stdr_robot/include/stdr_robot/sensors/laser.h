24a796e00ca14189202891eb9a8946e884874629
Aris Thallas <aris.thallas@gmail.com>
2013-12-18 22:14:05 +0200

    Changed laser header and code file indentation

--- stdr_robot/include/stdr_robot/sensors/laser.h
+++ stdr_robot/include/stdr_robot/sensors/laser.h
@@ -28,26 +28,26 @@
 
 namespace stdr_robot {
 
-class Laser : public Sensor {
-	
-	public:
-	
-		Laser(const nav_msgs::OccupancyGrid& map,
-				const geometry_msgs::Pose2DPtr& robotPosePtr,
-				tf::TransformBroadcaster& tf,
-				const stdr_msgs::LaserSensorMsg& msg, 
-				const std::string& name, 
-				ros::NodeHandle& n);
-		virtual void updateSensorCallback(const ros::TimerEvent&);
-		virtual void tfCallback(const ros::TimerEvent&);
-		
-		~Laser() {}
-		
-	private:
-		
-		stdr_msgs::LaserSensorMsg _description;
-};
-	
+  class Laser : public Sensor {
+
+    public:
+
+      Laser(const nav_msgs::OccupancyGrid& map,
+          const geometry_msgs::Pose2DPtr& robotPosePtr,
+          tf::TransformBroadcaster& tf,
+          const stdr_msgs::LaserSensorMsg& msg, 
+          const std::string& name, 
+          ros::NodeHandle& n);
+      virtual void updateSensorCallback(const ros::TimerEvent&);
+      virtual void tfCallback(const ros::TimerEvent&);
+
+      ~Laser() {}
+
+    private:
+
+      stdr_msgs::LaserSensorMsg _description;
+  };
+
 }
 
 #endif
