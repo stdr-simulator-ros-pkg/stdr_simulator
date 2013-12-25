9c50682e17dbbc3099ed746e591b060050c3bc86
Manos Tsardoulias <etsardou@gmail.com>
2013-12-25 20:17:39 +0200

    stdr_gui_loader documented

--- stdr_gui/include/stdr_gui/stdr_gui_loader.h
+++ stdr_gui/include/stdr_gui/stdr_gui_loader.h
@@ -25,40 +25,93 @@
 #include "ui_simulator.h"
 #include "stdr_gui/stdr_tools.h"
 
+/**
+@namespace stdr_gui
+@brief The main namespace for STDR GUI
+**/ 
 namespace stdr_gui
 {
-  
+  /**
+  @class GuiConnector
+  @brief Implements the low level Qt functionalities of the main window. Inherits from Ui_MainWindow (generated from an ui file) and QMainWindow. 
+  **/ 
   class CGuiLoader : public Ui_MainWindow, public QMainWindow
   {
+    //------------------------------------------------------------------------//
     private:
+      //!< The number of input arguments
       int   argc_;
+      //!< The input arguments
       char**  argv_;
       
+      //!< True if the exit signal was emmited
       bool   close_signal_;
+      //!< The exit event (when occurs)
       QCloseEvent   *event_;
+      
+    //------------------------------------------------------------------------//
     public:
-
+      
+      //!< The action of properties button
       QAction *actionProperties;
+      //!< The action of toggle grid button
       QAction *actionGrid;
+      //!< The action of creating new robot button
       QAction *actionNewRobot;
+      //!< The action of adding a new robot button 
       QAction *actionAddRobot;
+      //!< The action of new RFID tag button
       QAction *actionNewRfid;
+      //!< The action of new thermal source button
       QAction *actionNewThermal;
+      //!< The action of new CO2 source button
       QAction *actionNewCo2;
+      //!< The action of loading a map button
       QAction *actionLoadMap;
+      //!< The action of zooming in button
       QAction *actionZoomIn;
+      //!< The action of zooming out button
       QAction *actionZoomOut;
+      //!< The action of map adjusted button
       QAction *actionAdjusted;
     
+      /**
+      @brief Default contructor
+      @param argc [int] Number of input arguments
+      @param argv [char **] Input arguments
+      @return void
+      **/
       CGuiLoader(int argc,char **argv);
       
+      /**
+      @brief Overloading of closeEvent function from QMainWindow
+      @param event [QCloseEvent*] The exit event
+      @return void
+      **/
       void closeEvent(QCloseEvent *event);
       
+      /**
+      @brief Adds the tool buttons in the main window toolbar
+      @return void
+      **/
       void addToolbarIcons(void);
       
+      /**
+      @brief Returns the exit event
+      @return QEvent* 
+      **/
       QEvent* getCloseEvent(void);
+      
+      /**
+      @brief Returns true if a close event was triggered
+      @return bool
+      **/
       bool closeTriggered(void);
       
+      /**
+      @brief Shuts down the main window
+      @return void
+      **/
       void shutdown(void);
       
   };
