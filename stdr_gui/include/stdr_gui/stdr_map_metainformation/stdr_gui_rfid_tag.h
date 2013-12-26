4740fd93f3339f4593ce4156433c07428bc26d25
Manos Tsardoulias <etsardou@gmail.com>
2013-12-26 13:46:18 +0200

    stdr_gui_rfid_tag documented

--- stdr_gui/include/stdr_gui/stdr_map_metainformation/stdr_gui_rfid_tag.h
+++ stdr_gui/include/stdr_gui/stdr_map_metainformation/stdr_gui_rfid_tag.h
@@ -25,22 +25,73 @@
 
 #include "stdr_gui/stdr_tools.h"
 
+/**
+@namespace stdr_gui
+@brief The main namespace for STDR GUI
+**/ 
 namespace stdr_gui
 {
-
+  /**
+  @class CGuiRfidTag
+  @brief Implements the functionalities of an RFID tag
+  **/ 
   class CGuiRfidTag
   {
+    //------------------------------------------------------------------------//
     private:
-      QPoint position_;  
+      //!< The position of the rfid tag in the map
+      QPoint position_; 
+      //!< The "name" of the rfid tag 
       std::string name_;  
+      //!< The message of the rfid tag
       QString message_;
+    //------------------------------------------------------------------------//
     public:
+      /**
+      @brief Default contructor
+      @param p [QPoint] The pose of the rfid tag
+      @param name [std::string] The "name" of the rfid tag
+      @return void
+      **/
       CGuiRfidTag(QPoint p,std::string name);
+      
+      /**
+      @brief Default destructor
+      @return void
+      **/
       ~CGuiRfidTag(void);
+      
+      /**
+      @brief Returns the "name" of the rfid tag
+      @return std::string 
+      **/
       std::string getName(void);
+      
+      /**
+      @brief Checks proximity to a point
+      @param p [QPoint] The proximity point to check
+      @return bool : True if tag is close to p
+      **/
       bool checkProximity(QPoint p);
+      
+      /**
+      @brief Draws the tag in the map
+      @param img [QImage*] The image to draw to
+      @return void
+      **/
       void draw(QImage *img);
+      
+      /**
+      @brief Sets the tag message
+      @param msg [QString] The message to be set
+      @return void
+      **/
       void setMessage(QString msg);
+      
+      /**
+      @brief Returns the tag message
+      @return QString
+      **/
       QString getMessage(void);
   };  
 }
