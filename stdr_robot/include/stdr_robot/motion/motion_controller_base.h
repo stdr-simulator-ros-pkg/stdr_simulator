7a99f2fa45d394d7a120dc0185c8ae2c1e85870f
Manos Tsardoulias <etsardou@gmail.com>
2014-01-04 14:59:20 +0200

    exceptions,ideal_motion_controller,motion_cotroller_base documentation and c.s. fixed

--- stdr_robot/include/stdr_robot/motion/motion_controller_base.h
+++ stdr_robot/include/stdr_robot/motion/motion_controller_base.h
@@ -27,36 +27,85 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Pose2D.h>
 
+/**
+@namespace stdr_robot
+@brief The main namespace for STDR Robot
+**/ 
 namespace stdr_robot {
-	
-class MotionController {
-	
-	public:
-		
-		virtual void velocityCallback(const geometry_msgs::Twist& msg) = 0;
-		virtual void stop() = 0;
-		virtual void calculateMotion(const ros::TimerEvent& event) = 0;
-		
-		virtual ~MotionController() {}
-	
-	protected:
-		
-		MotionController(const geometry_msgs::Pose2DPtr& pose, tf::TransformBroadcaster& tf, const std::string& name)
-		: _posePtr(pose), _tfBroadcaster(tf), _freq(0.1), _namespace(name) { }
-	
-	protected:
-		
-		const std::string& _namespace;
-		ros::Subscriber _velocitySubscrider;
-		ros::Duration _freq;
-		ros::Timer _calcTimer;
-		tf::TransformBroadcaster& _tfBroadcaster;
-		const geometry_msgs::Pose2DPtr& _posePtr;
-		geometry_msgs::Twist _currentTwist;
-};
-	
-typedef boost::shared_ptr<MotionController> MotionControllerPtr;
-	
+    
+  /**
+  @class MotionController
+  @brief Abstract class that provides motion controller abstraction
+  **/ 
+  class MotionController {
+    
+    public:
+      
+      /**
+      @brief Pure virtual function - Callback for velocity commands
+      @param msg [const geometry_msgs::Twist&] The velocity command
+      @return void
+      **/
+      virtual void velocityCallback(const geometry_msgs::Twist& msg) = 0;
+      
+      /**
+      @brief Pure virtual function - Stops the robot
+      @return void
+      **/
+      virtual void stop(void) = 0;
+      
+      /**
+      @brief Pure virtual function - Calculates the motion - updates the robot pose
+      @param event [const ros::TimerEvent&] A ROS timer event
+      @return void
+      **/
+      virtual void calculateMotion(const ros::TimerEvent& event) = 0;
+      
+      /**
+      @brief Default desctructor
+      @return void
+      **/
+      virtual ~MotionController(void) 
+      {
+      }
+    
+    protected:
+      
+      /**
+      @brief Default constructor
+      @param pose [const geometry_msgs::Pose2DPtr&] The robot pose
+      @param tf [tf::TransformBroadcaster&] A ROS tf broadcaster
+      @param name [const std::string&] The robot frame id
+      @return void
+      **/
+      MotionController(
+        const geometry_msgs::Pose2DPtr& pose, 
+        tf::TransformBroadcaster& tf, 
+        const std::string& name)
+          : _posePtr(pose), _tfBroadcaster(tf), _freq(0.1), _namespace(name) 
+      { 
+      }
+    
+    protected:
+      
+      //!< The base of the frame_id
+      const std::string& _namespace;
+      //!< ROS subscriber to the velocity topic
+      ros::Subscriber _velocitySubscrider;
+      //!< Frequency of motion calculation
+      ros::Duration _freq;
+      //!< ROS timer for generating motion calculation events
+      ros::Timer _calcTimer;
+      //!< Broadcaster of the robot tf transform
+      tf::TransformBroadcaster& _tfBroadcaster;
+      //!< Robot pose message
+      const geometry_msgs::Pose2DPtr& _posePtr;
+      //!< Current motion command
+      geometry_msgs::Twist _currentTwist;
+  };
+    
+  typedef boost::shared_ptr<MotionController> MotionControllerPtr;
+    
 }
 
 
