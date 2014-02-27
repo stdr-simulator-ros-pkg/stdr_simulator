c04616d6c83d2e88855b9cc332a067d0bb7811b2
Manos Tsardoulias <etsardou@gmail.com>
2014-02-27 17:44:56 +0200

    Minor changes. Added message when relocation is not possible.

--- stdr_gui/src/stdr_gui/stdr_gui_loader.cpp
+++ stdr_gui/src/stdr_gui/stdr_gui_loader.cpp
@@ -76,7 +76,7 @@ namespace stdr_gui
     actionAddRobot = new QAction(this);
     actionAddRobot->setObjectName(QString::fromUtf8("actionNewRobot"));
     actionAddRobot->setCheckable(false);
-    actionAddRobot->setIconText(QString("Add robot"));
+    actionAddRobot->setIconText(QString("Load robot"));
     QIcon iconAddRobot;
     iconAddRobot.addFile(QString::fromUtf8((
       stdr_gui_tools::getRosPackagePath("stdr_gui") + 
