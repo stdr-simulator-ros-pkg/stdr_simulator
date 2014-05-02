e781ed0147ba781e7ec0e0820b9ad68cfd91d925
Manos Tsardoulias <etsardou@gmail.com>
2014-05-02 12:00:37 +0300

    Added robot-side support - not tested yet

---
+++ stdr_robot/src/sensors/microphone.cpp
@@ -0,0 +1,162 @@
+/******************************************************************************
+   STDR Simulator - Simple Two DImensional Robot Simulator
+   Copyright (C) 2013 STDR Simulator
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+   
+   Authors : 
+   * Manos Tsardoulias, etsardou@gmail.com
+   * Aris Thallas, aris.thallas@gmail.com
+   * Chris Zalidis, zalidis@gmail.com 
+******************************************************************************/
+
+#include <stdr_robot/sensors/microphone.h>
+
+namespace stdr_robot {
+  
+  /**
+  @brief Default constructor
+  @param map [const nav_msgs::OccupancyGrid&] An occupancy grid map
+  @param msg [const stdr_msgs::SoundSensorMsg&] The sensor description message
+  @param name [const std::string&] The sensor frame id without the base
+  @param n [ros::NodeHandle&] The ROS node handle
+  @return void
+  **/ 
+  SoundSensor::SoundSensor(
+    const nav_msgs::OccupancyGrid& map,
+    const stdr_msgs::SoundSensorMsg& msg, 
+    const std::string& name,
+    ros::NodeHandle& n)
+    : Sensor(map, name, n, msg.pose, msg.frame_id, msg.frequency)
+  {
+    _description = msg;
+
+    _publisher = n.advertise<stdr_msgs::SoundSensorMeasurementMsg>
+      ( _namespace + "/" + msg.frame_id, 1 );
+      
+    sound_sources_subscriber_ = n.subscribe(
+      "stdr_server/sound_sources_list", 
+      1, 
+      &SoundSensor::receiveSoundSources,
+      this);
+  }
+  
+  /**
+  @brief Default destructor
+  @return void
+  **/ 
+  SoundSensor::~SoundSensor(void)
+  {
+    
+  }
+
+  /**
+  @brief Updates the sensor measurements
+  @return void
+  **/ 
+  void SoundSensor::updateSensorCallback() 
+  {
+    if (sound_sources_.sound_sources.size() == 0) return;    
+
+    stdr_msgs::SoundSensorMeasurementMsg measuredSourcesMsg;
+
+    measuredSourcesMsg.header.frame_id = _description.frame_id;
+
+    
+    //~ float max_range = _description.maxRange;
+    //~ float sensor_th = tf::getYaw(_sensorTransform.getRotation());
+    //~ float min_angle = sensor_th - _description.angleSpan / 2.0;
+    //~ float max_angle = sensor_th + _description.angleSpan / 2.0;
+    //~ 
+    //~ //!< Must implement the functionality
+    //~ for(unsigned int i = 0 ; i < rfid_tags_.rfid_tags.size() ; i++)
+    //~ {
+      //~ //!< Check for max distance
+      //~ float sensor_x = _sensorTransform.getOrigin().x();
+      //~ float sensor_y = _sensorTransform.getOrigin().y();
+      //~ float dist = sqrt(
+        //~ pow(sensor_x - rfid_tags_.rfid_tags[i].pose.x, 2) +
+        //~ pow(sensor_y - rfid_tags_.rfid_tags[i].pose.y, 2)
+      //~ );
+      //~ if(dist > max_range)
+      //~ {
+        //~ continue;
+      //~ }
+      //~ 
+      //~ //!< Check for correct angle
+      //~ float ang = atan2(rfid_tags_.rfid_tags[i].pose.y - sensor_y,
+        //~ rfid_tags_.rfid_tags[i].pose.x - sensor_x);
+      //~ 
+      //~ if(!angCheck(ang, min_angle, max_angle))
+      //~ {
+        //~ continue;
+      //~ }
+      //~ 
+      //~ measuredTagsMsg.rfid_tags_ids.push_back(rfid_tags_.rfid_tags[i].tag_id);
+      //~ measuredTagsMsg.rfid_tags_msgs.push_back(rfid_tags_.rfid_tags[i].message);
+      //~ measuredTagsMsg.rfid_tags_dbs.push_back(1.0); //!< Needs to change into a realistic measurement
+    //~ }
+    
+    measuredSourcesMsg.header.stamp = ros::Time::now();
+    measuredSourcesMsg.header.frame_id = _namespace + "_" + _description.frame_id;
+    _publisher.publish( measuredSourcesMsg );
+  }
+  
+  /**
+  @brief Receives the existent sources
+  **/
+  void SoundSensor::receiveSoundSources(const stdr_msgs::SoundSourceVector& msg)
+  {
+    sound_sources_ = msg;
+  }
+  
+  /**
+  @brief Checks if an angle is between two others. Supposes that min < max
+  @param target_ [float] The target angle
+  @param min_ [float] min angle
+  @param max_ [float] max angle
+  @return true on success
+  **/ 
+  static bool angCheck(float target_, float min_, float max_) 
+  {
+    int c = 0;
+    c = (target_ + 2 * PI) / (2 * PI);
+    float target = target_ + (1 - c) * PI * 2;
+    c = (min_ + 2 * PI) / (2 * PI);
+    float min = min_ + (1 - c) * PI * 2;
+    c = (max_ + 2 * PI) / (2 * PI);
+    float max = max_ + (1 - c) * PI * 2;
+    
+    if(min_ * max_ > 0) //!< Same sign
+    {
+      if(target > min && target < max)
+      {
+        return true;
+      }
+    }
+    else
+    {
+      max += 2 * PI;
+      if(target > min && target < max)
+      {
+        return true;
+      }
+      target += 2 * PI;
+      if(target > min && target < max)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+}  // namespace stdr_robot
---
+++ stdr_robot/src/sensors/thermal.cpp
@@ -0,0 +1,158 @@
+/******************************************************************************
+   STDR Simulator - Simple Two DImensional Robot Simulator
+   Copyright (C) 2013 STDR Simulator
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+   
+   Authors : 
+   * Manos Tsardoulias, etsardou@gmail.com
+   * Aris Thallas, aris.thallas@gmail.com
+   * Chris Zalidis, zalidis@gmail.com 
+******************************************************************************/
+
+#include <stdr_robot/sensors/thermal.h>
+
+namespace stdr_robot {
+  
+  /**
+  @brief Default constructor
+  **/ 
+  ThermalSensor::ThermalSensor(
+    const nav_msgs::OccupancyGrid& map,
+    const stdr_msgs::ThermalSensorMsg& msg, 
+    const std::string& name,
+    ros::NodeHandle& n)
+    : Sensor(map, name, n, msg.pose, msg.frame_id, msg.frequency)
+  {
+    _description = msg;
+
+    _publisher = n.advertise<stdr_msgs::ThermalSensorMeasurementMsg>
+      ( _namespace + "/" + msg.frame_id, 1 );
+      
+    thermal_sources_subscriber_ = n.subscribe(
+      "stdr_server/thermal_sources_list", 
+      1, 
+      &ThermalSensor::receiveThermalSources,
+      this);
+  }
+  
+  /**
+  @brief Default destructor
+  @return void
+  **/ 
+  ThermalSensor::~ThermalSensor(void)
+  {
+    
+  }
+
+  /**
+  @brief Updates the sensor measurements
+  @return void
+  **/ 
+  void ThermalSensor::updateSensorCallback() 
+  {
+    if (thermal_sources_.thermal_sources.size() == 0) return;    
+
+    stdr_msgs::ThermalSensorMeasurementMsg measuredSourcesMsg;
+
+    measuredSourcesMsg.header.frame_id = _description.frame_id;
+
+    
+    //~ float max_range = _description.maxRange;
+    //~ float sensor_th = tf::getYaw(_sensorTransform.getRotation());
+    //~ float min_angle = sensor_th - _description.angleSpan / 2.0;
+    //~ float max_angle = sensor_th + _description.angleSpan / 2.0;
+    //~ 
+    //~ //!< Must implement the functionality
+    //~ for(unsigned int i = 0 ; i < rfid_tags_.rfid_tags.size() ; i++)
+    //~ {
+      //~ //!< Check for max distance
+      //~ float sensor_x = _sensorTransform.getOrigin().x();
+      //~ float sensor_y = _sensorTransform.getOrigin().y();
+      //~ float dist = sqrt(
+        //~ pow(sensor_x - rfid_tags_.rfid_tags[i].pose.x, 2) +
+        //~ pow(sensor_y - rfid_tags_.rfid_tags[i].pose.y, 2)
+      //~ );
+      //~ if(dist > max_range)
+      //~ {
+        //~ continue;
+      //~ }
+      //~ 
+      //~ //!< Check for correct angle
+      //~ float ang = atan2(rfid_tags_.rfid_tags[i].pose.y - sensor_y,
+        //~ rfid_tags_.rfid_tags[i].pose.x - sensor_x);
+      //~ 
+      //~ if(!angCheck(ang, min_angle, max_angle))
+      //~ {
+        //~ continue;
+      //~ }
+      //~ 
+      //~ measuredTagsMsg.rfid_tags_ids.push_back(rfid_tags_.rfid_tags[i].tag_id);
+      //~ measuredTagsMsg.rfid_tags_msgs.push_back(rfid_tags_.rfid_tags[i].message);
+      //~ measuredTagsMsg.rfid_tags_dbs.push_back(1.0); //!< Needs to change into a realistic measurement
+    //~ }
+    
+    measuredSourcesMsg.header.stamp = ros::Time::now();
+    measuredSourcesMsg.header.frame_id = _namespace + "_" + _description.frame_id;
+    _publisher.publish( measuredSourcesMsg );
+  }
+  
+  /**
+  @brief Receives the existent sources
+  **/
+  void ThermalSensor::receiveThermalSources(
+    const stdr_msgs::ThermalSourceVector& msg)
+  {
+    thermal_sources_ = msg;
+  }
+  
+  /**
+  @brief Checks if an angle is between two others. Supposes that min < max
+  @param target_ [float] The target angle
+  @param min_ [float] min angle
+  @param max_ [float] max angle
+  @return true on success
+  **/ 
+  static bool angCheck(float target_, float min_, float max_) 
+  {
+    int c = 0;
+    c = (target_ + 2 * PI) / (2 * PI);
+    float target = target_ + (1 - c) * PI * 2;
+    c = (min_ + 2 * PI) / (2 * PI);
+    float min = min_ + (1 - c) * PI * 2;
+    c = (max_ + 2 * PI) / (2 * PI);
+    float max = max_ + (1 - c) * PI * 2;
+    
+    if(min_ * max_ > 0) //!< Same sign
+    {
+      if(target > min && target < max)
+      {
+        return true;
+      }
+    }
+    else
+    {
+      max += 2 * PI;
+      if(target > min && target < max)
+      {
+        return true;
+      }
+      target += 2 * PI;
+      if(target > min && target < max)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+}  // namespace stdr_robot
